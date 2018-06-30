04814b996695aa919efb62f5c9a8e627c9762db8
Pedro Gabaldon <34518201+PeterGabaldon@users.noreply.github.com>
2018-06-30 12:26:36 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -13,25 +13,31 @@
 import setuptools
 
 requires=["oauth2client", "httplib2", "google-api-python-client"]
-scripts = "DriveUtil"
+
+if platform.system() == "Windows":
+	SCRIPTS = ['bin/DriveUtil.bat', 'bin/DriveUtil']
+else:
+	SCRIPTS = ['bin/DriveUtil']
+	
+	
 
 with open("README.md", "r") as f:
     long_description = f.read()
 
 setuptools.setup(
 	name="DriveUtility",
-	version="0.0.5",
+	version="0.0.8",
 	author="Pedro Gabaldon Julia",
 	author_email="petergj@protonmail.com",
 	description="Google Drive tool",
 	long_description=long_description,
     long_description_content_type="text/markdown",
 	url="https://github.com/PeterGabaldon/DriveUtility",
-	scripts=["bin/DriveUtil.py"],
+	scripts=scripts,
 	license="MIT",
-	include_package_data=True,
 	install_requires=requires,
 	packages=setuptools.find_packages(),
+	include_package_data=True,
 	classifiers=(
 		"Programming Language :: Python :: 2.7",
 		"License :: OSI Approved :: MIT License",
